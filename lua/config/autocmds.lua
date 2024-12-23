e760f6369b96da5a7b37767fe03b0c7eb5354a4f
Peter Czibik <p.czibik@gmail.com>
2024-12-23 12:13:09 +0100

    Add autocommand for autoimport

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.ts", "*.tsx" }, -- TypeScript files
+  callback = LazyVim.lsp.action["source.addMissingImports.ts"],
+  desc = "Auto-add missing imports on save for TypeScript files",
+})
